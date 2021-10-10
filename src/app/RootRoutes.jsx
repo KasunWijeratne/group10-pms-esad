6dc834556820b6cc5556df83926e7f735784c292
KasunWijeratne <wijeratna.e.kasun@gmail.com>
2021-10-10 19:03:37 +0530

    Develop branch

--- src/app/RootRoutes.jsx
+++ src/app/RootRoutes.jsx
@@ -3,14 +3,6 @@ import { Redirect } from 'react-router-dom'
 
 import dashboardRoutes from './views/dashboard/DashboardRoutes'
 
-import materialRoutes from './views/material-kit/MaterialRoutes'
-import chartsRoute from './views/charts/ChartsRoute'
-import dragAndDropRoute from './views/Drag&Drop/DragAndDropRoute'
-
-import formsRoutes from './views/forms/FormsRoutes'
-import mapRoutes from './views/map/MapRoutes'
-
-
 const redirectRoute = [
     {
         path: '/',
@@ -27,11 +19,6 @@ const errorRoute = [
 
 const routes = [
     ...dashboardRoutes,
-    ...materialRoutes,
-    ...chartsRoute,
-    ...dragAndDropRoute,
-    ...formsRoutes,
-    ...mapRoutes,
     ...redirectRoute,
     ...errorRoute,
 ]
